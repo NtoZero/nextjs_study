56f836e864fcc1062265eb44b072de535171160c
st.lee <dev.st1027@gmail.com>
2025-01-12 02:54:11 +0900

    feat(S2. Page Router 핵심 정리) : 2-4 프리페칭 - 동적 네비게이션에도 프리패칭 적용 및 프리페칭 강제 해제

--- s2/src/pages/_app.tsx
+++ s2/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -12,11 +13,23 @@ export default function App({ Component, pageProps }: AppProps) {
     // router.back("/test"); // 뒤로 가기
   };
 
+  /* test 페이지는 동적 네비게이팅이므로 프리페칭이 되지 않는다.
+   * 해결 방법은 다음과 같다.
+   * => _app.tsx가 렌더링 될 때 pre-fetching 하도록 동적으로 설계한다.
+   * */
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header>
         <Link href={"/"}>index</Link> &nbsp;
-        <Link href={"/search"}>search</Link> &nbsp;
+        {/*링크 컴포넌트 search에 대한 강제 prefetching 해제*/}
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>{" "}
+        &nbsp;
         <Link href={"/book/1"}>book</Link> &nbsp;
         <div>
           <button onClick={onClickButton}>/test페이지로 이동</button>
