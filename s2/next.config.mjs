052a77b9e2998958278260f7dd80f2392672c935
st.lee <dev.st1027@gmail.com>
2025-01-12 02:41:01 +0900

    feat(S2. Page Router 핵심 정리) : 2-4 프리페칭 - 운영모드 실행 초기 세팅

--- s2/next.config.mjs
+++ s2/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, /*25.01.02 : 개발에서 엄격 모드 사용 설정 false로 변경*/
+  reactStrictMode: false /*25.01.02 : 개발에서 엄격 모드 사용 설정 false로 변경*/,
 };
 
 export default nextConfig;
--- s2/src/pages/search/index.tsx
+++ s2/src/pages/search/index.tsx
@@ -6,6 +6,7 @@ export default function Page() {
   // console.log(router);
   // const q = router.query.q; // 객체의 쿼리스트링 이름 'q' 꺼내오기
   const { q } = router.query; // 구조분해할당을 통해 꺼내오기
+  console.log(q);
 
   return <h1>search</h1>;
 }
