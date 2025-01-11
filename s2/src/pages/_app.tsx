092531532ffe5ad2928c59761220bd54b168272d
st.lee <dev.st1027@gmail.com>
2025-01-12 01:52:30 +0900

    feat(S2. Page Router 핵심 정리) : 2-3 네비게이팅 - 페이지 라우팅

--- s2/src/pages/_app.tsx
+++ s2/src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    // router.push("/test"); // 앞으로 이동
+    router.replace("/test"); // 뒤로 가기를 방지하면서 앞으로 이동
+    // router.back("/test"); // 뒤로 가기
+  };
+
   return (
     <>
-      <header>공통 헤더</header> {/*공통 헤더를 입력할 수도 있음*/}
+      <header>
+        <Link href={"/"}>index</Link> &nbsp;
+        <Link href={"/search"}>search</Link> &nbsp;
+        <Link href={"/book/1"}>book</Link> &nbsp;
+        <div>
+          <button onClick={onClickButton}>/test페이지로 이동</button>
+        </div>
+      </header>
+      {/*공통 헤더를 입력할 수도 있음*/}
       <Component {...pageProps} />
     </>
   );
