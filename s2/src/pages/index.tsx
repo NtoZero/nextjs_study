c3f34da593bbad2b76bafc1a3668de92494f3163
stae <dev.st1027@gmail.com>
2025-01-12 12:45:18 +0900

    feat(S2. Page Router 핵심 정리) : 2-5 api_routes

--- s2/src/pages/index.tsx
+++ s2/src/pages/index.tsx
@@ -1,7 +1,15 @@
+/*S2-6. NEXT에서는 페이지별 클래스 이름을 원천 차단하기 위하여 CSS 충돌로 사용할 수 없음. */
+// import "./index.module.css";
+
+/* CSS Module 임포트*/
+import style from "./index.module.css";
+
 export default function Home() {
   return (
     <>
-      <h1>인덱스</h1>
+      {/*  CSS Module 임포트하여 className 동적 생성 */}
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
     </>
   );
 }
