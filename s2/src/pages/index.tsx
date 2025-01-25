96cc27e980f4e594748d8b9b1fad8019550ed2e9
st.lee <dev.st1027@gmail.com>
2025-01-26 00:14:14 +0900

    feat(S2. Page Router 핵심 정리) : 2-11. 서버사이드렌더링 1

--- s2/src/pages/index.tsx
+++ s2/src/pages/index.tsx
@@ -4,11 +4,38 @@
 /* CSS Module 임포트*/
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+/* 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수 */
+export const getServerSideProps = () => {
+  const data = "hello";
+
+  // console.log("서버사이드프롭스예요"); // 서버에서만 동작. 브라우저에서 확인 불가.
+  // console.log(window);
+
+  /*
+  주의 사항:
+  1. 단 하나의 객체를 반환해야 한다.
+  2. 객체 내부에 props라는 이름의 프로퍼티를 가지고 있어야 한다.
+  */
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
+
+  useEffect(() => {
+    /*useEffect를 사용하면 브라우저에 컴포넌트 마운트 이후에만 사용 */
+    console.log(window);
+  }, []);
+
   return (
     <div className={style.container}>
       <section>
