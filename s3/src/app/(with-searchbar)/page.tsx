8c46d48d8230b29b086200cf1f82b018fd668af9
st.lee <dev.st1027@gmail.com>
2025-01-29 21:53:35 +0900

    feat(S3. App Router 시작하기) : 3.4. 리액트 서버 컴포넌트 이해하기 (1) 인덱스 페이지 실험

--- s3/src/app/(with-searchbar)/page.tsx
+++ s3/src/app/(with-searchbar)/page.tsx
@@ -1,10 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      인덱스 페이지
-    </div>
-  );
+  console.log("Home 컴포넌트 입니다.");
+
+  return <div className={styles.page}>인덱스 페이지</div>;
 }
