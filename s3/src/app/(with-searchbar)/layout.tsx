c8af9554db714e21e9ab37f063d01e01a7183f27
st.lee <dev.st1027@gmail.com>
2025-01-30 13:03:58 +0900

    feat(S3. App Router 시작하기) : 3.6.네비게이션 - 실습2. 프로그래머틱 페이지 이동 (서치 바 루트 layout로 옮김)

--- s3/src/app/(with-searchbar)/layout.tsx
+++ s3/src/app/(with-searchbar)/layout.tsx
@@ -5,11 +5,6 @@ import SearchBar from "@/app/component/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <header>
-        <Link href={"/"}>index</Link> &nbsp;
-        <Link href={"/search"}>search</Link> &nbsp;
-        <Link href={"/book/1"}>book/1</Link> &nbsp;
-      </header>
       <div>
         <SearchBar />
         {children}
