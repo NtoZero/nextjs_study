15f69f441ff818484c594e91365b669d53181c62
st.lee <dev.st1027@gmail.com>
2025-01-30 20:58:04 +0900

    feat(S4. 서버컴포넌트와 데이터페칭): 4.1. - 실습 적용 👉 Best-Practice는 book 상세 페이지 참조

--- s4/src/app/(with-searchbar)/search/page.tsx
+++ s4/src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,27 @@
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
   searchParams: {
     q?: string;
   };
 }) {
+  // `searchParams`가 동기적으로 제공되지 않을 수도 있으므로 안전하게 처리
+  const params = await Promise.resolve(searchParams);
+  const q = params.q || "";
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_HOST}/book/search?q=${q}`
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const books: BookData[] = await response.json();
+
   return (
     <div>
       {books.map((book) => (
