d2e1c15e90d11c83a78317793cc01745c494b9f2
st.lee <dev.st1027@gmail.com>
2025-02-01 21:05:53 +0900

    feat(S5. 페이지 캐싱) : 5.5. 클라이언트 라우터 캐시 - 캐싱 여부 확인하기

--- s5/src/app/(with-searchbar)/layout.tsx
+++ s5/src/app/(with-searchbar)/layout.tsx
@@ -4,6 +4,8 @@ import Searchbar from "../../components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      {/* 클라이언트 라우트 캐시는 새로고침 또는 탭종료 시 제거된다. */}
+      {/*<div>{new Date().toLocaleString()}</div>*/}
       <Suspense fallback={<div> Loading... </div>}>
         <Searchbar />
       </Suspense>
