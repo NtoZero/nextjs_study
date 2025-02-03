aa8fadcd1ce762a1a792a82b9191c1f2cbb7d7bd
st.lee <dev.st1027@gmail.com>
2025-02-03 22:32:28 +0900

    feat(S7. 서버 액션): 7.2. 리뷰 추가 기능 구현하기

---
+++ s7/src/actions/create-review.action.ts
@@ -0,0 +1,33 @@
+"use server";
+export async function createReviewAction(formData: FormData) {
+  /* 서버 액션 사용*/
+
+  const bookId = formData.get("bookId")?.toString();
+  const content = formData.get("content")?.toString();
+  const author = formData.get("author")?.toString();
+
+  if (!bookId || !content || !author) {
+    return;
+  }
+
+  console.log(`${process.env.NEXT_PUBLIC_API_HOST}/review`);
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/review`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        bookId,
+        content,
+        author,
+      }),
+    });
+
+    console.log(`response.status : ${response.status}`);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
--- s7/src/app/book/[id]/page.tsx
+++ s7/src/app/book/[id]/page.tsx
@@ -1,5 +1,6 @@
 import style from "./page.module.css";
 import { notFound } from "next/navigation";
+import { createReviewAction } from "@/actions/create-review.action";
 
 // export const dynamicParams = false;
 
@@ -37,21 +38,13 @@ async function BookDetail({ bookId }: { bookId: string }) {
   );
 }
 
-function ReviewEditor() {
-  async function createReviewAction(formData: FormData) {
-    /* 서버 액션 사용*/
-    "use server";
-
-    const content = formData.get("content")?.toString();
-    const author = formData.get("author")?.toString();
-
-    console.log(content, author);
-  }
+function ReviewEditor({ bookId }: { bookId: string }) {
   return (
     <section>
       <form action={createReviewAction}>
-        <input name="content" placeholder="리뷰 내용" />
-        <input name="author" placeholder="작성자" />
+        <input name="bookId" value={bookId} hidden readOnly />
+        <input name="content" required placeholder="리뷰 내용" />
+        <input name="author" required placeholder="작성자" />
         <button type="submit">작성하기</button>
       </form>
     </section>
@@ -75,7 +68,7 @@ export default async function Page({
   return (
     <div>
       <BookDetail bookId={id} />
-      <ReviewEditor />
+      <ReviewEditor bookId={id} />
     </div>
   );
 }
