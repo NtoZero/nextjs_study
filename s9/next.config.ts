efdde61b29a53a0e7065b3b929e02964b8ad1dcc
st.lee <dev.st1027@gmail.com>
2025-02-11 21:57:23 +0900

    feat(S9. 최적화와 배포): 9.1. 이미지 최적화 - 이미지 최적화

--- s9/next.config.ts
+++ s9/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "shopping-phinf.pstatic.net", // 허용할 이미지 도메인
+        pathname: "**", // 모든 경로 허용
+      },
+    ],
+  },
   logging: {
     fetches: {
       fullUrl: true,
